d432452057249daf44b8d2c99b61f0818f3f78e5
Dio Kim <dykim335@gmail.com>
2023-02-09 17:04:16 +0900

    first commit

---
+++ workspace/practice4.py
@@ -0,0 +1,21 @@
+# 리스트: 순서를 가지는 객체의 집합
+# 예) 지하철 칸 별로 10명, 20명, 30명이 있을 때
+subway1 = 10
+subway2 = 20
+subway3 = 30
+# 위와 같이 작성, 위의 변수를 하나로 작성할 수 있다
+subway = [10, 20, 30] # subway 리스트 안에 10, 20, 30 값이 차례로 들어감
+print(subway)
+
+subway = ["John", "Anna", "Mary"] # 순서는 0 1 2
+print(subway)
+
+# 활용
+# Anna가 몇번 째 칸에 있는가
+print(subway.index("Anna")) # 1
+
+# 사전
+# 튜플
+# 세트
+# 자료구조의 변경
+# Quiz
