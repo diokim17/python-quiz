f3444b835c28967964e79f287f06084073a078f7
Dio Kim <124035614+diokim17@users.noreply.github.com>
2023-03-22 23:43:03 +0900

    Update quiz1-10.py

--- quiz1-10.py
+++ quiz1-10.py
@@ -1,1 +1,59 @@
 # 1번 ~ 10번
+
+# 1번 화면에 Hello World 문자열을 출력하시오.
+print("Hello World")
+
+# >> Hello World
+
+
+# 2번 화면에 Mary's cosmetics을 출력하시오.
+print("Mary's cosmetics")
+
+# >> Mary's cosmetics
+
+
+# 3번 화면에 신씨가 소리질렀다. "도둑이야". 를 출력하시오.
+print('신씨가 소리질렀다. "도둑이야".')
+
+# >> 신씨가 소리질렀다. "도둑이야".
+
+
+# 4번 화면에 "C:\Windows" 를 출력하시오.
+print("C:\Windows")
+
+# >> C:\Windows
+
+
+# 5번 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보시오. print("안녕하세요. \n만나서\t\t반갑습니다.")
+# >> 안녕하세요.
+#    만나서    반가워요.
+# \n은 줄 바꿈을 실행하고, \t는 공백, 띄어쓰기를 나타냄 -> 여기서 공백은 스페이스 바가 아닌 탭으로 실행
+
+
+# 6번 print("오늘은", "일요일") 의 출력 결과를 예상해보시오.
+
+# >> 오늘은 월요일
+
+
+# 7번 print()함수를 사용하여 naver;kakao;sk;samsung을 출력하시오.
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# >> naver;kakao;sk;samsung
+
+
+# 8번 print()함수를 사용하여 naver/kakao/sk/samsung을 출력하시오.
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# >> naver/kakao/sk/samsung
+
+
+# 9번 print("first");print("second") 코드를 수정하여 줄바꿈 없이 출력하시오. (단, print()함수는 2번 사용할 것)
+print("first", end="");print("second")
+
+# >> firstsecond
+
+
+# 10번 5/3의 결과를 출력하시오.
+print(5/3)
+
+# >> 1.6666666
